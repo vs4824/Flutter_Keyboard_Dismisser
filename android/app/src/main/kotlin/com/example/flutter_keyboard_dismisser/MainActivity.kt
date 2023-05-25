f3c372662162f9564b4661be6dc3d7eda29d177d
Vaibhav Sharma <vaibhav.2022@tecorb.co>
2023-05-25 18:21:33 +0530

    Flutter Keyboard Dismisser

---
+++ android/app/src/main/kotlin/com/example/flutter_keyboard_dismisser/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.flutter_keyboard_dismisser
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,45 @@
+import 'package:flutter/material.dart';
+import 'package:keyboard_dismisser/keyboard_dismisser.dart';
+
+void main() => runApp(ExampleApp());
+
+class ExampleApp extends StatelessWidget {
+  const ExampleApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) => MaterialApp(
+    title: 'Keyboard Dismisser',
+    theme: ThemeData(primarySwatch: Colors.blue),
+    home: ExamplePage(),
+  );
+}
+
+class ExamplePage extends StatelessWidget {
+  const ExamplePage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) => KeyboardDismisser(
+    gestures: [
+      GestureType.onTap,
+      GestureType.onPanUpdateDownDirection,
+    ],
+    child: Scaffold(
+      appBar: AppBar(
+        title: Text('KeyboardDismisser example'),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.symmetric(
+          horizontal: 24,
+          vertical: 48,
+        ),
+        child: TextField(
+          decoration: InputDecoration(
+            border: OutlineInputBorder(),
+            labelText: 'Tap to show the keyboard',
+            hintText: 'Tap elsewhere or swipe down to dismiss',
+          ),
+        ),
+      ),
+    ),
+  );
+}
